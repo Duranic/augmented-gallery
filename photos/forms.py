2f4002dd8b41ef09b87502ab63c7a5040a6a5427
Duranic <dominik.duranic@etfos.hr>
2023-06-15 11:14:14 +0200

    styled form

--- photos/forms.py
+++ photos/forms.py
@@ -7,3 +7,15 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
